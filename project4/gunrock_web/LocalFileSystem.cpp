b4241ea3129fa48aff2eff43a94eb9911029c975
Travis <liangtravis910@gmail.com>
2024-11-18 15:48:34 -0800

    start project 4

--- project4/gunrock_web/LocalFileSystem.cpp
+++ project4/gunrock_web/LocalFileSystem.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <assert.h>
 
@@ -8,60 +9,74 @@
 
 using namespace std;
 
-
-LocalFileSystem::LocalFileSystem(Disk *disk) {
+LocalFileSystem::LocalFileSystem(Disk *disk)
+{
   this->disk = disk;
 }
 
-void LocalFileSystem::readSuperBlock(super_t *super) {
-
+void LocalFileSystem::readSuperBlock(super_t *super)
+{
+  vector<unsigned char> buffer(UFS_BLOCK_SIZE);
+  this->disk->readBlock(0, buffer.data());
+  memcpy(super, buffer.data(), sizeof(super_t));
 }
 
-void LocalFileSystem::readInodeBitmap(super_t *super, unsigned char *inodeBitmap) {
-
+void LocalFileSystem::readInodeBitmap(super_t *super, unsigned char *inodeBitmap)
+{
+  for (int i = 0; i < super->inode_bitmap_len; i++)
+  {
+    vector<unsigned char> buffer(UFS_BLOCK_SIZE);
+    this->disk->readBlock(super->inode_bitmap_start + i, buffer.data());
+    memcpy(inodeBitmap + i * UFS_BLOCK_SIZE, buffer.data(), UFS_BLOCK_SIZE);
+  }
 }
 
-void LocalFileSystem::writeInodeBitmap(super_t *super, unsigned char *inodeBitmap) {
-
+void LocalFileSystem::writeInodeBitmap(super_t *super, unsigned char *inodeBitmap)
+{
 }
 
-void LocalFileSystem::readDataBitmap(super_t *super, unsigned char *dataBitmap) {
-
+void LocalFileSystem::readDataBitmap(super_t *super, unsigned char *dataBitmap)
+{
 }
 
-void LocalFileSystem::writeDataBitmap(super_t *super, unsigned char *dataBitmap) {
-
+void LocalFileSystem::writeDataBitmap(super_t *super, unsigned char *dataBitmap)
+{
 }
 
-void LocalFileSystem::readInodeRegion(super_t *super, inode_t *inodes) {
-
+void LocalFileSystem::readInodeRegion(super_t *super, inode_t *inodes)
+{
 }
 
-void LocalFileSystem::writeInodeRegion(super_t *super, inode_t *inodes) {
-
+void LocalFileSystem::writeInodeRegion(super_t *super, inode_t *inodes)
+{
 }
 
-int LocalFileSystem::lookup(int parentInodeNumber, string name) {
+int LocalFileSystem::lookup(int parentInodeNumber, string name)
+{
   return 0;
 }
 
-int LocalFileSystem::stat(int inodeNumber, inode_t *inode) {
+int LocalFileSystem::stat(int inodeNumber, inode_t *inode)
+{
   return 0;
 }
 
-int LocalFileSystem::read(int inodeNumber, void *buffer, int size) {
+int LocalFileSystem::read(int inodeNumber, void *buffer, int size)
+{
   return 0;
 }
 
-int LocalFileSystem::create(int parentInodeNumber, int type, string name) {
+int LocalFileSystem::create(int parentInodeNumber, int type, string name)
+{
   return 0;
 }
 
-int LocalFileSystem::write(int inodeNumber, const void *buffer, int size) {
+int LocalFileSystem::write(int inodeNumber, const void *buffer, int size)
+{
   return 0;
 }
 
-int LocalFileSystem::unlink(int parentInodeNumber, string name) {
+int LocalFileSystem::unlink(int parentInodeNumber, string name)
+{
   return 0;
 }
-
